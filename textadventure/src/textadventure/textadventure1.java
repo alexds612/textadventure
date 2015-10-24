d7a0f93e0415f4fdfbb857a27e9889189a46eda3
Nickstreme <nicobravo1@hotmail.com>
2015-10-23 21:02:11 -0400

    Created the main files

---
+++ textadventure/src/textadventure/textadventure1.java
@@ -0,0 +1,9 @@
+package textadventure;
+import java.util.Scanner;
+
+public class textadventure1 {
+	public static void main(String args[]) {
+		System.out.print("Test");
+		Scanner input = new Scanner(System.in);
+	}
+}
